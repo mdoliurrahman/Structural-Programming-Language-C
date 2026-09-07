cb129de1757153b04de8e8b631ef7c3394ba53a0
MD Oliur Rahman <mdoliurrahman593@gmail.com>
2026-09-07 09:22:47 +0300

    Add files via upload

---
+++ DataTypes.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int studentID=101;
+    float score=99.5768;
+    char grade='A';
+    printf("Student ID:%d\nScore: %.2f\nGrade: %c",studentID,score,grade);
+    return 0;
+}
