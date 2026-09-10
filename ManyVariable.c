e1987cc717489b98028ddc11353e943b30a38062
MD Oliur Rahman <mdoliurrahman593@gmail.com>
2026-09-10 11:28:53 +0600

    Add files via upload

---
+++ ManyVariable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ int main()
+ {
+
+ int MyNumber=19;
+ float Myfriend=19.27;
+ char others='A';
+  printf("%d\n",MyNumber);
+  printf("%f\n",Myfriend);
+  printf("%c\n",others);
+     return 0;
+
+
+
+
+ }
