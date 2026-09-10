950dc40fb5fb5a66aade6a95f1247a1bf86c3b89
MD Oliur Rahman <mdoliurrahman593@gmail.com>
2026-09-10 12:23:57 +0600

    Add files via upload

---
+++ SizeOf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+  {
+ int myInt;
+ float myFloat;
+ double myDouble;
+ char myChar;
+      printf("%zu", sizeof myInt);
+      printf("%zu", sizeof myFloat);
+      printf("%zu", sizeof myDouble);
+      printf("%zu", sizeof myChar);
+
+
+
+  return 0;
+  }
