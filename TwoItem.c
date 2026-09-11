7fddd9ea38e00387d3f0812675909cdcb532e6cf
MD Oliur Rahman <mdoliurrahman593@gmail.com>
2026-09-12 00:05:57 +0600

    Add files via upload

---
+++ TwoItem.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+
+  {
+  int items=10;
+  float cost_per_item=12.50;
+  float total_items=items*cost_per_item;
+  char currency='$';
+
+  total_items='items*cost_per_item';
+    printf(" THE NUMBER OF ITEMS=%d\n",items);
+    printf("TOTAL COST FOR EACH ITEM=%.2f%c\n ",cost_per_item,currency);
+    printf ("currentcy=%.2f",currency);
+
+    return 0;
+     }
