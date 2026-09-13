1ed70b4fba5fedbb936b7c8a08f65c0b3ecc8f1e
MD Oliur Rahman <mdoliurrahman593@gmail.com>
2026-09-13 20:11:30 +0600

    Add files via upload

---
+++ LogicalSign.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+  int a=1;
+      int b=0;
+  printf("a is highest=%d\n",a&&!b);
+  printf("b has positive =%d\n",b||a);
+  printf("not access=%d",a);
+
+
+    return 0;
+
+
+
+
+        }
