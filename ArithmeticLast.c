7ab19a97f0e42b86cff59457bff4b93af67ea8ac
MD Oliur Rahman <mdoliurrahman593@gmail.com>
2026-09-13 19:51:25 +0600

    Add files via upload

---
+++ ArithmeticLast.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+  int x,y;
+  x=50;
+  y=7;
+  printf("%d\n",x/y);
+  float a,b;
+       a=50;
+       b=7;
+       printf("%f\n",a/b);
+    return 0;
+
+
+
+
+        }
