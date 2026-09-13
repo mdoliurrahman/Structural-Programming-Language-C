3ef3d5bb7f729f43b0a130ea157fea351eda7c3f
MD Oliur Rahman <mdoliurrahman593@gmail.com>
2026-09-13 21:43:03 +0600

    Add files via upload

---
+++ IfElseifElse.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int mynumber=50;
+    if (mynumber>0)
+      printf("THIS IS A POSITIVE NUMBER");
+    else if (mynumber<0)
+        printf("THIS IS NAGATIVE NUMBER");
+    else printf(" IT IS ZERO");
+
+
+
+
+    return 0;
+
+
+
+
+        }
