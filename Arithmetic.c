43a5e6848f58325f4a5b35c2d0f65f6248759d8d
MD Oliur Rahman <mdoliurrahman593@gmail.com>
2026-09-13 19:41:12 +0600

    Add files via upload

---
+++ Arithmetic.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+
+  int x=50;
+  int y=10;
+
+      printf("%d\n",x+y);
+      printf("%d\n",x-y);
+      printf("%d\n",x*y);
+      printf("%d\n",x/y);
+      printf("%d\n",x%y);
+
+
+        int z=40;
+        ++z;
+        printf("%d\n",z);
+        --z;
+        printf("d\n",z);
+        return 0;
+
+
+
+
+        }
