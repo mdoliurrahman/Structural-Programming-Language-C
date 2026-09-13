c5b4697e9d20aa3a2c2412dabd591b0c58807d8d
MD Oliur Rahman <mdoliurrahman593@gmail.com>
2026-09-13 22:00:20 +0600

    Add files via upload

---
+++ Switch.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main(){
+    int day=4;
+ switch(day)  {
+
+ case 1:
+     printf("Saturday");
+  break;
+
+  case 2:
+     printf("Sunday");
+   break;
+
+   case3:
+
+     printf("Monday");
+   break;
+   case 4:
+     printf("Tuesday");
+   break;
+   case 5:
+    printf("wednessday");
+    break;
+
+   case 6:
+    printf("thuesday");
+
+    break;
+   case 7:
+    printf("friday");
+    break;
+
+     return 0;
+
+     }
+
+
+
+
+        }
