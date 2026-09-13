85e8e9c15448bb2bb8763ae28af047494aebba21
MD Oliur Rahman <mdoliurrahman593@gmail.com>
2026-09-13 20:59:25 +0600

    Add files via upload

---
+++ BoolAndIfElse.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+
+    int time=18;
+
+
+  bool isDay=time>10;
+  if(isDay)
+
+
+    {printf("THIS IS GOOD DAY");
+    }
+
+else{ ("THIS IS PAINFUL MOMENT");
+
+}
+    return 0;
+
+
+
+
+        }
