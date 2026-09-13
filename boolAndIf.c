ab5d31b09059604e8da31bac736ae0fbd0140c10
MD Oliur Rahman <mdoliurrahman593@gmail.com>
2026-09-13 20:49:04 +0600

    Add files via upload

---
+++ boolAndIf.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+
+  int x=50;
+  int y=100;
+
+ bool isGreater = y>x;
+ if (isGreater)
+    {printf("Y is Greater than X");
+
+
+
+
+ }
+
+
+    return 0;
+
+
+
+
+        }
