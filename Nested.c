32cfa870a8d03fa3cdac3357f378bb322d745592
MD Oliur Rahman <mdoliurrahman593@gmail.com>
2026-09-15 21:43:14 +0600

    Add files via upload

---
+++ Nested.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+
+  int i,j;
+  for (i=0;i<=10;i=i+2)
+
+{
+    printf("%d\n",i);
+
+}
+
+  for(j=15;j<50;j=j+5)
+  {  printf("%d\n",j);
+  }
+
+
+  return 0;
+     }
